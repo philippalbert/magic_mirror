2a0b922885119b27e4db725a5181fca1eb1f8436
Philipp Albert <philipp.albert@merckgroup.com>
2025-09-26 16:41:43 +0200

    Update model

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def ask():
                 'Content-Type': 'application/json'
             },
             json={
-                'model': 'llama3-8b-8192',
+                'model': 'llama-3.1-8b-instant',
                 'messages': [{'role': 'user', 'content': question}],
                 'max_tokens': 150
             }
